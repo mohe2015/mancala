4db572bb70bb7b24d4817514f2e652b7fe1a3ade
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-07-06 22:34:23 +0200

    Jo

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ use std::fmt::Display;
 struct Mancala {
     board: [[u8; 16]; 2],
     /*
+player 0
      0  1  2  3  4  5  6  7
     15 14 13 12 11 10  9  8
     31 30 29 28 27 26 25 24
     16 17 18 19 20 21 22 23
+player 1
 
      0  1  2  3  4  5  6  7
     15 14 13 12 11 10  9  8
@@ -26,14 +28,14 @@ impl Mancala {
     }
 
     // Mtaji phase
-    fn mtaji_phase(&mut self, player: usize, position: usize, direction: usize) {
+    fn mtaji_phase(&mut self, player: usize, mut position: usize, mut direction: isize) {
         assert!(self.board[player][position] >= 2);
         // sowing
         let mut i = position;
         let mut count = self.board[player][position];
         self.board[player][position] = 0;
         while count > 0 {
-            i += direction % 16;
+            i = ((i as isize + direction) % 16) as usize;
             self.board[player][i] += 1;
             count -= 1;
         }
@@ -44,12 +46,19 @@ impl Mancala {
             self.board[1 - player][position] = 0;
             // if capture in kimbi and "right" kichwa
             if position == 8 || position == 9 {
-
+                // counter clockwise
+                direction = if player == 1 { 1 } else { -1 };
+                position = 8;
             }
             // if capture in kimbi and "left" kichwa
-            if position == 14 || position == 15 {
-
+            else if position == 14 || position == 15 {
+                // clockwise
+                direction = if player == 1 { -1 } else { 1 };
+                position = 15;
+            } else {
+                // chose kichwa
             }
+            // sowing again
         } else {
             // mtaji turn
 
